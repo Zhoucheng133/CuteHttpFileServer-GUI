12c7266ae0bd72bfcdf13e31c441173c3bf5e1c5
周诚 <zhoucheng133@outlook.com>
2023-05-06 21:11:00 +0800

    更新版本号

--- lib/main.dart
+++ lib/main.dart
@@ -387,7 +387,7 @@ class InfoPage extends StatelessWidget {
         SizedBox(
           height: 50,
         ),
-        Text("Version: 1.1.0"),
+        Text("Version: 1.1.1"),
         SizedBox(
           height: 50,
         ),
